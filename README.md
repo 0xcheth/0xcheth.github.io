05edcc99be3278fc1b6c7ad59b28716cfe4c2633
0xcheth <chethaka123@gmail.com>
2024-11-30 00:32:21 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+uhhh I don't know what im doing here either... =p
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '0xcheth';
+export const SITE_DESCRIPTION = 'Chethaka Ladduwahettys online portfolio and blog';
