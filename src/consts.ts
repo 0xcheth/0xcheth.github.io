f59b24a66666ec213ab4979ca5d84be7955aeae3
0xcheth <chethaka123@gmail.com>
2024-11-30 21:29:52 +1100

    damm I cant pull from the repo L

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,5 @@
 
 export const SITE_TITLE = '0xcheth';
 export const SITE_DESCRIPTION = 'Online portfolio for Chethaka Ladduwahetty and blog';
+
 export const SITE_LOGO = '/src/assets/images/favicon.svg';
