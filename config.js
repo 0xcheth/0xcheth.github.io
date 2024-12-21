5e84890b7fda2ec41eda85c05d117f3209948a77
0xcheth <chethaka123@gmail.com>
2024-12-21 22:57:22 +1100

    finally fixed an age old bug lol hamburger menue is now perfect

---
+++ config.js
@@ -0,0 +1,28 @@
+// Add your website or pages link eg. 'https://username.github.io'
+export const Yourweblink = {
+    SiteURL:'https://notnehah.github.io',
+};
+
+// Path type. Dont change this if you dont know what it is.
+export const Yourpathtype = "/"  //'/' For root-level deployment, '/subpath/' for deployment in a subpath
+
+// Add your website name eg. "0xcheth"
+export const Yourwebname = "Neha.a";
+
+// Add your full name/brand eg. John Doe
+export const Yourname = "Neha Aftab";
+
+// Add your email eg.user123@gmail.com
+export const Youremail = "n.a.k1@outlook.com"; 
+
+
+// Add your linkedin link eg. "https://www.linkedin.com/in/user123"
+export const Yourlinkedin = "https://www.linkedin.com/in/neha-aftab-5219a31a9/"; 
+
+
+// Add your Instagram link eg. "https://instagram.com/user123"
+export const Yourinstagram = "https://www.instagram.com/notnehah/"; 
+
+
+// Add your github link eg. "https://github.com/0xcheth"
+export const Yourgithub = "https://github.com/notnehah/"; 
