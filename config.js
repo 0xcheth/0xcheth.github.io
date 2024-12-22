cfe47661edbd2ed5e63d96c1d225fe857241953b
0xcheth <chethaka123@gmail.com>
2024-12-22 12:16:46 +1100

    new skin lol

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
+//
+//
+//
+// Social and minor website details
+//
+//
+//
+
 // Add your website or pages link eg. 'https://username.github.io'
 export const Yourweblink = {
     SiteURL:'https://notnehah.github.io',
@@ -7,22 +15,54 @@ export const Yourweblink = {
 export const Yourpathtype = "/"  //'/' For root-level deployment, '/subpath/' for deployment in a subpath
 
 // Add your website name eg. "0xcheth"
-export const Yourwebname = "Neha.a";
+export const Yourwebname = "0xcheth.astro"
 
-// Add your full name/brand eg. John Doe
-export const Yourname = "Neha Aftab";
-
-// Add your email eg.user123@gmail.com
-export const Youremail = "n.a.k1@outlook.com"; 
-
+// Add your full name/brand for copyright statement eg. John Doe
+export const Yourname = "0xcheth";
 
 // Add your linkedin link eg. "https://www.linkedin.com/in/user123"
-export const Yourlinkedin = "https://www.linkedin.com/in/neha-aftab-5219a31a9/"; 
+export const Yourlinkedin = "https://www.linkedin.com/in/0xcheth/"; 
 
 
 // Add your Instagram link eg. "https://instagram.com/user123"
-export const Yourinstagram = "https://www.instagram.com/notnehah/"; 
+export const Yourinstagram = "https://www.instagram.com/cheth17/"; 
 
 
 // Add your github link eg. "https://github.com/0xcheth"
-export const Yourgithub = "https://github.com/notnehah/"; 
+export const Yourgithub = "https://github.com/0xcheth/"; 
+
+//
+//
+//
+// CSS COLOR STYLING
+//
+//
+//
+
+export const Themecolors = {
+
+    HeaderBg: "#FFFFFF", // Background color for the header
+
+    MainBg: "#FFFFFF", // Background color for the main body
+
+    HeaderText: "auto", // Text color for the headers
+
+    MobileNav: "#2C2F33", // Background color for the mobile navigation menu
+
+    MobileNavBtn: "transparent", // Text color for the mobile navigation buttons
+
+    NavBtnHover: "#orange", // Text color for the mobile navigation buttons
+
+    HeadingstxtCl: "#FFFFFF", // Text color for the headings
+
+    BodytextCl: "#99AAB5", // Text color for the body text
+
+    ListCl: "lightgrey", // Text color for the list
+
+    HyperlinkCl: "#7289DA", // Text color for the hyperlinks
+
+    HyperlinkClHover: "#5865F2", // Text color for the hyperlinks on hover
+
+    CodeBlockBg: "#2C2F33", // Background color for the code blocks
+
+};
